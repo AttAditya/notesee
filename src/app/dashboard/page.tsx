5d0d51ca2b8c43b8f9042e36666a1d8b76be6951
AttAditya <attachment.aditya@gmail.com>
2025-04-23 20:00:05 +0530

    responsiveness when no notes

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,7 +5,7 @@ import { useContext } from "react";
 import { Note } from "@/components/Note"
 import { AppSidebar } from "@/components/AppSidebar"
 import { NotesContext } from "@/providers/notes"
-import { SidebarProvider } from "@/components/ui/sidebar";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 
 export default function Dashboard() {
   const { activeNote, updateNote } = useContext(NotesContext);
@@ -15,6 +15,14 @@ export default function Dashboard() {
       <div className="flex flex-1">
         <SidebarProvider>
           <AppSidebar />
+          {!activeNote && (
+            <div className="h-full w-full p-4 flex">
+              <div className="flex gap-2 h-min items-center">
+                <SidebarTrigger className="cursor-pointer" />
+                Open/Create a note from Sidebar to get started!
+              </div>
+            </div>
+          )}
           {activeNote && (
             <Note
               key={activeNote.id}
