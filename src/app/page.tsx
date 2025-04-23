ddd940184502baff4822241e6f04c6368ad83933
AttAditya <attachment.aditya@gmail.com>
2025-04-23 19:31:42 +0530

    Landing Revamp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,44 @@
+import { ChevronRight, Notebook, User, Zap } from "lucide-react";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1 className="text-4xl sm:text-5xl font-bold tracking-[-.01em] text-center sm:text-left">
-          Summarize your Notes in seconds!
+    <div className="w-screen h-screen flex justify-center items-center">
+      <main className="flex flex-col gap-2 items-center">
+        <h1 className="text-8xl">
+          Notesee
         </h1>
 
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Login with your Google account
+        <h2 className="text-3xl text-center font-light">
+          Easing summarizing notes
+        </h2>
+
+        <ul className="flex gap-10 m-16 items-center">
+          <li className="w-80 bg-black/5 rounded-2xl p-8 flex flex-col items-center gap-4">
+            <User className="rounded-full w-10 h-10 bg-black text-white p-3" />
+            Login to Notesee
           </li>
-          <li className="mb-2 tracking-[-.01em]">
+          <ChevronRight />
+          <li className="w-80 bg-black/5 rounded-2xl p-8 flex flex-col items-center gap-4">
+            <Notebook className="rounded-full w-10 h-10 bg-black text-white p-3" />
             Write/paste your notes
           </li>
-          <li className="tracking-[-.01em]">
+          <ChevronRight />
+          <li className="w-80 bg-black/5 rounded-2xl p-8 flex flex-col items-center gap-4">
+            <Zap className="rounded-full w-10 h-10 bg-black text-white p-3" />
             Run the summarizer
           </li>
-        </ol>
+        </ul>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a href="/auth/login">
             <button className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto cursor-pointer">
               Login
+            </button>
+          </a>
+
+          <a href="/auth/register">
+            <button className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto cursor-pointer">
+              Register
             </button>
           </a>
         </div>
